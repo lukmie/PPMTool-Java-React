0de41ac99d2321ee53c0817592d969990995fea3
lukmie <lmienicki@gmail.com>
2020-01-19 21:29:01 +0100

    Repo added Project entity set up

--- PPMTool/src/main/java/com/lukmie/ppmtool/domain/Project.java
+++ PPMTool/src/main/java/com/lukmie/ppmtool/domain/Project.java
@@ -1,7 +1,35 @@
 package com.lukmie.ppmtool.domain;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
 
 @Data
+@Entity
+@NoArgsConstructor
 public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String projectName;
+    private String projectIdentifier;
+    private String description;
+    private Date startDate;
+    private Date endDate;
+    private Date createdAt;
+    private Date updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = new Date();
+    }
+
 }
